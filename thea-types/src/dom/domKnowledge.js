0fa6dc96e48997c7609188b884b12f283cf8c57c
Alexander <idokutela@gmail.com>
2017-04-13 18:37:53 +0200

    DOM Knowledge prototypes

---
+++ thea-types/src/dom/domKnowledge.js
@@ -0,0 +1,47 @@
+import escape from 'escape-html';
+import voidElements from './voidElements';
+import { setToString, getEventName } from '../TheaDOM';
+import reduce from '../util/reduce';
+
+export { default as voidElements } from './voidElements';
+export const rawTextElements = new Set(['SCRIPT', 'STYLE']);
+export const escapableRawTextElements = new Set(['TEXTAREA', 'TITLE']);
+
+export const booleanAttributes = new Set(['allowfullscreen',
+  'async', 'autofocus', 'autoplay', 'checked', 'compact',
+  'controls', 'declare', 'default', 'defaultchecked', 'defaultmuted',
+  'defaultselected', 'defer', 'disabled', 'draggable', 'enabled',
+  'formnovalidate', 'hidden', 'indeterminate', 'inert', 'ismap',
+  'itemscope', 'loop', 'multiple', 'muted', 'nohref', 'noresize',
+  'noshade', 'novalidate', 'nowrap', 'open', 'pauseonexit', 'readonly',
+  'required', 'reversed', 'scoped', 'seamless', 'selected', 'sortable',
+  'spellcheck', 'translate', 'truespeed', 'typemustmatch', 'visible']);
+
+/* http://stackoverflow.com/questions/8955533/javascript-jquery-split-camelcase-string-and-add-hyphen-rather-than-space */
+export const camelToDash = s => s.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+const concatStyleItem = (r, [k, v]) => `${r};${camelToDash(k)}:${String(v)}`;
+const styleToString = styles => (styles.size ?
+  `style="${reduce(styles.entries(), concatStyleItem, '')}"` : '');
+
+const concatS = (s, t) => ((s.length && s[s.length - 1] !== '"') ? `${s} ${t}` : s + t);
+
+function concatAttrString(r, k, v) {
+  if (getEventName(k)) return r;
+  if (booleanAttributes.has(k)) {
+    return (v !== undefined) ? concatS(r, k) : r;
+  }
+  return concatS(r, `${k}="${escape(String(v))}"`);
+}
+const attrsToString = attrs => reduce(attrs.entries, concatAttrString, '');
+
+export function toStringNoDOM(tagName, attrMap, styleMap, childString) {
+  const styles = styleToString(styleMap);
+  const attrs = styles ? concatS(attrsToString(attrMap), styles) : attrsToString(attrMap);
+  if (voidElements.has(tagName)) {
+    return `<${tagName} ${attrs}/>`;
+  }
+  return `<${tagName} ${attrs}>${childString}</${tagName}>`;
+}
+
+setToString(toStringNoDOM);
